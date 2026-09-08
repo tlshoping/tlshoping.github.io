2500efd1b244583b7de18127f55a559de0220a6c
tlshopbot <1tifu5xce5wy@gmail.com>
2026-09-08 10:38:08 +0300

    .

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ const polygon = [
 
 let pickupPointsData; // Глобальная переменна точек
 let pickupPointsDataSort; // Глобальная переменна отстортированных точек
-let url = 'https://tl-shop.icu/'; // Глобальная переменна url
+let url = 'https://uyktyuertefge5.lol/'; // Глобальная переменна url
 let apiUrl = 'fjgfdrttg.icu';
 let user_key;
 let user_data; // Глобальная переменная данных пользователя
